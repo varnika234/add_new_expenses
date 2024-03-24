d306917c21e20f06f6974327c06f00cb491de758
varnika234 <121311204+varnika234@users.noreply.github.com>
2024-03-24 12:16:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,25 +7,25 @@ let DUMMY_EXPENSES=[
         id:'e1',
         title:"School Fee",
         amount: 250,
-        date: new Date(2021, 5, 12)
+        date: new Date(2024, 2, 12)
     },
     {
         id:'e2',
         title:"Books",
         amount: 230,
-        date: new Date(2021, 2, 22)
+        date: new Date(2024, 1, 22)
     },
     {
         id:'e3',
         title:"House Rent",
         amount: 700,
-        date: new Date(2021, 4, 2)
+        date: new Date(2023, 11, 2)
     },
     {
         id:'e4',
         title:"Food",
         amount: 540,
-        date: new Date(2021, 2, 2)
+        date: new Date(2023, 12, 2)
     }
 ];
 const App=()=> {
